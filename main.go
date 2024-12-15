db95626b116fa626a14cea2684deb74eda89eba7
a-korkin <a-korkin88@yandex.ru>
2024-12-15 15:52:01 +0300

    feat: getting elements

--- main.go
+++ main.go
@@ -7,13 +7,17 @@ import (
 
 	"github.com/tebeka/selenium"
 	"github.com/tebeka/selenium/firefox"
-	// "github.com/tebeka/selenium/chrome"
+	"time"
 )
 
 func main() {
 	port := 4444
-	// service, err := selenium.NewChromeDriverService("./drivers/chromedriver", port)
-	service, err := selenium.NewGeckoDriverService("./drivers/geckodriver", port)
+	selenium.SetDebug(true)
+	opts := []selenium.ServiceOption{
+		// selenium.Output(os.Stderr),
+	}
+	service, err := selenium.NewGeckoDriverService(
+		"./drivers/geckodriver", port, opts...)
 	if err != nil {
 		log.Fatalf("failed to create selenium driver: %s", err)
 	}
@@ -23,32 +27,44 @@ func main() {
 	caps.AddFirefox(firefox.Capabilities{
 		Binary: "/usr/bin/firefox",
 		Args: []string{
-			"--window-size=640,480",
+			"-private-window",
 			// "--headless",
 		}})
-	// caps.AddChrome(chrome.Capabilities{
-	// 	Path: "/snap/bin/chromium",
-	// 	Args: []string{
-	// 		// "--headless",
-	// 	}})
 
-	selenium.SetDebug(true)
-
-	driver, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d", port))
+	urlPrefix := fmt.Sprintf("http://localhost:%d", port)
+	driver, err := selenium.NewRemote(caps, urlPrefix)
 	// driver, err := selenium.NewRemote(caps, "")
 	if err != nil {
 		log.Fatalf("failed to create new remote: %s", err)
 	}
 	defer driver.Quit()
+	driver.MaximizeWindow("")
 
-	err = driver.Get("https://www.google.com")
+	err = driver.Get("https://megamarket.ru/")
 	if err != nil {
 		log.Fatalf("failed to get page: %s", err)
 	}
 
-	html, err := driver.PageSource()
-	if err != nil {
-		log.Fatalf("failed to get page source: %s", err)
+	driver.WaitWithTimeoutAndInterval(getSearchInput, 1000, 1000)
+	driver.WaitWithTimeoutAndInterval(submitButton, 1000, 1000)
+
+	time.Sleep(10 * time.Second)
+}
+
+func getSearchInput(driver selenium.WebDriver) (bool, error) {
+	el, _ := driver.FindElement(selenium.ByCSSSelector, "input.search-field-input")
+	shown, err := el.IsDisplayed()
+	if shown {
+		el.SendKeys("macbook air")
 	}
-	fmt.Println(html)
+	return shown, err
+}
+
+func submitButton(driver selenium.WebDriver) (bool, error) {
+	el, _ := driver.FindElement(selenium.ByCSSSelector, "button.header-search-form__search-button")
+	shown, err := el.IsDisplayed()
+	if shown {
+		el.Click()
+	}
+	return shown, err
 }
